1c79863b8a17ebf3d02974716e76f565367ff3ed
beka3301 <bekturhanazarov98@gmail.com>
2022-12-12 18:58:12 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Scanner scc = new Scanner(System.in);
+        Scanner sccf = new Scanner(System.in);
+
+        System.out.print("first number: ");
+        int firstNumber = sc.nextInt();
+
+        System.out.print("Operation: ");
+        String  operation = scc.nextLine();
+
+        System.out.print("last number: ");
+        int lastNumber = sc.nextInt();
+
+        System.out.print("Operation: ");
+        String operetion1=sccf.nextLine();
+
+        plus(firstNumber,operation,lastNumber);
+
+    }
+
+    public static void plus(int a, String  t, int b) {
+        if (t.equals("+")){
+            System.out.println(a + b);
+        } else if (t.equals("-")) {
+            System.out.println(a-b);
+        } else if (t.equals("*")) {
+            System.out.println(a*b);
+        } else if (t.equals("/")) {
+            System.out.println(a/b);
+        }
+
+    }
+}
+
